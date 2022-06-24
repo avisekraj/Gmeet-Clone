5890a05d09d170e5fbf3ac579678b53ce14441aa
Abhishek <araj14250@gmail.com>
2022-06-24 14:16:01 +0530

    do

---
+++ app/config/redis.js
@@ -0,0 +1,15 @@
+let redis = require("redis");
+var options = {
+  host: process.env.REDIS_HOST,
+  port: process.env.REDIS_PORT,
+  auth_pass: process.env.REDIS_PASSWORD,
+};
+
+let client = redis.createClient(options);
+
+
+client.on("error", (error) => {
+  console.log(error);
+});
+
+module.exports = client;
---
+++ client/src/components/UI/Header/Header.js
@@ -0,0 +1,23 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faQuestionCircle,
+  faExclamationCircle,
+  faCog,
+} from "@fortawesome/free-solid-svg-icons";
+import "./Header.scss";
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="logo">
+        <img src="https://res.cloudinary.com/donyn5cnn/image/upload/t_media_lib_thumb/v1656047995/u2iwlth2ptmslx4pspvj.png" />
+        <span className="help-text">Meet</span>
+      </div>
+      <div className="action-btn">
+        <FontAwesomeIcon className="icon-block" icon={faQuestionCircle} />
+        <FontAwesomeIcon className="icon-block" icon={faExclamationCircle} />
+        <FontAwesomeIcon className="icon-block" icon={faCog} />
+      </div>
+    </div>
+  );
+};
+export default Header;
